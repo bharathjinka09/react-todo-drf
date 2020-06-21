ba0d673c0aeab956a9ddfe9f6e2260ba0e082817
bharathjinka09 <bharathjinka09@gmail.com>
2020-06-21 18:20:10 +0530

    Added React app with Django rest API

---
+++ frontend/src/App.js
@@ -0,0 +1,207 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component{
+
+  constructor(props){
+    super (props);
+    this.state = {
+      todoList:[],
+      activeItem:{
+        id:null,
+        title: '',
+        completed: false,
+      },
+      editing: false,
+    }
+    this.fetchTasks = this.fetchTasks.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.getCookie = this.getCookie.bind(this);
+    this.startEdit = this.startEdit.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.strikeUnstrike = this.strikeUnstrike.bind(this);
+  };
+
+  getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
+
+  componentWillMount(){
+    this.fetchTasks();
+  }
+
+  fetchTasks(){
+    console.log('Fetching tasks...')
+
+    fetch('https://bharath-todo.herokuapp.com/api/task-list/')
+    .then(response => response.json())
+    .then(data => 
+        this.setState({
+          todoList: data
+        })
+      )
+  }
+
+  handleChange(e){
+    var name = e.target.name
+    var value = e.target.value
+    console.log(name)
+    console.log(value)
+
+    this.setState({
+      activeItem:{
+        ...this.state.activeItem,
+        title: value
+      }
+    })
+  }
+
+  handleSubmit(e){
+    e.preventDefault()
+    console.log('Item',this.state.activeItem)
+
+    var csrftoken = this.getCookie('csrftoken');
+
+    var url = 'https://bharath-todo.herokuapp.com/api/task-create/'
+    
+    if(this.state.editing == true){
+      url = `https://bharath-todo.herokuapp.com/api/task-update/${this.state.activeItem.id}/`
+      this.setState({
+        editing: false
+      })
+    }
+
+
+
+
+    fetch(url,{
+      method: 'POST',
+      headers: {
+        'Content-Type':'application/json',
+        'X-CSRFToken': csrftoken,
+      },
+      body: JSON.stringify(this.state.activeItem)
+    }).then(response => {
+          this.fetchTasks()
+          this.setState({
+            activeItem:{
+                id:null,
+                title: '',
+                completed: false,
+              }
+          })
+    }).catch(error => console.log(error))
+  }
+
+  startEdit(task){
+      this.setState({
+        activeItem:task,
+        editing: true,
+      })
+  }
+
+  deleteItem(task){
+    var csrftoken = this.getCookie('csrftoken')
+
+    fetch(`https://bharath-todo.herokuapp.com/api/task-delete/${task.id}/`,{
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRFToken': csrftoken,
+      },
+    }).then(response => {
+      this.fetchTasks()
+    })
+
+  }
+
+  strikeUnstrike(task){
+    task.completed = !task.completed
+    var csrftoken = this.getCookie('csrftoken')
+    var url = `https://bharath-todo.herokuapp.com/api/task-update/${task.id}/`
+
+    fetch(url,{
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRFToken': csrftoken,
+      },
+      body: JSON.stringify({'completed': task.completed, 'title':task.title})
+    }).then(response => this.fetchTasks())
+
+    console.log(task.completed)
+  }
+
+  render(){
+    var tasks = this.state.todoList
+    var self = this
+    return(
+        <div className='container'>
+            <div className='text-center text-white mt-5'>
+              <h2>ToDo App</h2>
+            </div>
+          <div id='task-container'>
+            <div id='form-wrapper'>
+                  <form onSubmit={this.handleSubmit} id="form">
+                    <div className="flex-wrapper">
+                      <div style={{flex: 6}}>
+                        <input onChange={this.handleChange} id="title" className="form-control" type="text" value={this.state.activeItem.title} name="title" placeholder="Add Task" />
+                      </div>
+                      <div style={{flex: 1}}>
+                        <input id="submit" className="btn btn-outline-primary" type="submit" name='Add' />
+                      </div>
+                    </div>
+                  </form>
+            </div>
+
+            <div id='list-wrapper'>
+              {tasks.map((task,index) => (
+  
+                    <div key = { index } className='task-wrapper flex-wrapper'>
+                      
+                      <div onClick={() => self.strikeUnstrike(task)} style={ { flex:7 }}>
+                        { task.completed === false ? ( 
+                          <span>{task.title}</span>
+                          ) : (
+                          <strike>{task.title}</strike>
+
+                          )}
+                      </div>
+
+                      <div style={ { flex:1 }}>
+                        <button onClick={() => self.startEdit(task)} className='btn btn-sm btn-primary'><i className='fa fa-pencil'></i></button>
+                      </div>
+
+                      <div style={ { flex:1 }}>
+                        <button onClick={() => self.deleteItem(task)} className='btn btn-sm btn-danger'>X</button>
+                      </div>
+                    
+                    </div>
+
+                ))}
+            </div>
+
+
+          </div>
+        
+        </div>
+      )
+    }
+}
+
+
+export default App;
